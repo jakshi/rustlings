c090ca66b0607de4c3b155d27cd2f32d94ad2645
Kostiantyn Lysenko <kostiantyn@lysenko.dev>
2025-11-02 13:59:49 +0700

    add: solutions to all exercises
    why? Because I have completed all the exercises in the Rustlings course
    and want to commit my solutions to the repository for future reference
    and showing off :D

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = Vec::new();
+
+        for (string, command) in input {
+            let transformed_string = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => {
+                    let mut result = string.clone();
+                    for _ in 0..n {
+                        result.push_str("bar");
+                    }
+                    result
+                }
+            };
+            output.push(transformed_string);
+        }
+
+        output
+    }
 }
 
 fn main() {
@@ -39,6 +58,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
